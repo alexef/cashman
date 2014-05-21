79a7e1ecc1a88a49cc007ffdaa45bdf958797bb4
Alex Eftimie <alex@eftimie.ro>
2014-05-21 20:44:06 +0300

    admin: add a basic admin interface for wallets and categories

---
+++ cashman/admin.py
@@ -0,0 +1,111 @@
+from flask.views import View
+from flask import Blueprint, render_template, request, flash, redirect, url_for
+from wtforms.ext.sqlalchemy.orm import model_form
+from cashman.models import Wallet, db, Category
+
+
+admin = Blueprint('admin', __name__)
+admin.registered = []
+
+
+def get_list_url(model):
+    return url_for('.%s_list' % model)
+
+
+def get_edit_url(model, id):
+    return url_for('.%s_edit' % model, id=id)
+
+
+def get_delete_url(model, id):
+    return url_for('.%s_delete' % model, id=id)
+
+
+class ModelAdmin(View):
+
+    template_list = 'admin/list.html'
+    template_edit = 'admin/edit.html'
+    template_confirm = 'admin/confirm.html'
+
+    @classmethod
+    def name(cls):
+        return cls.model.__tablename__
+
+    @classmethod
+    def list(cls):
+        return render_template(
+            cls.template_list,
+            object_list=cls.model.query.all(),
+            model=cls.name(),
+        )
+
+    @classmethod
+    def edit(cls, id):
+        object = cls.model.query.get_or_404(id)
+        form_cls = model_form(cls.model)
+        form = form_cls(request.form, object)
+        if request.method == 'POST':
+            if form.validate():
+                form.populate_obj(object)
+                db.session.commit()
+                flash('Saved', 'success')
+
+        return render_template(
+            cls.template_edit,
+            object=object,
+            form=form,
+            model=cls.name(),
+        )
+
+    @classmethod
+    def delete(cls, id):
+        object = cls.model.query.get_or_404(id)
+
+        if request.method == 'POST':
+            db.session.delete(object)
+            db.session.commit()
+            flash('Deleted', 'success')
+            return redirect(get_list_url(cls.name()))
+        return render_template(
+            cls.template_confirm,
+            object=object,
+            model=cls.name(),
+        )
+
+    @classmethod
+    def register(cls, app):
+        model = cls.model.__tablename__
+        app.route('/%s/list/' % model, endpoint='%s_list' % model)(cls.list)
+        app.route('/%s/<int:id>/edit/' % model,
+                  endpoint='%s_edit' % model,
+                  methods=('GET', 'POST'))(cls.edit)
+        app.route('/%s/<int:id>/del/' % model,
+                  endpoint='%s_delete' % model,
+                  methods=('GET', 'POST'))(cls.delete)
+        admin.registered.append(model)
+
+
+class WalletAdmin(ModelAdmin):
+    model = Wallet
+
+
+class CategoryAdmin(ModelAdmin):
+    model = Category
+
+
+WalletAdmin.register(admin)
+CategoryAdmin.register(admin)
+
+
+@admin.app_context_processor
+def inject():
+    return dict(
+        get_list_url=get_list_url,
+        get_edit_url=get_edit_url,
+        get_delete_url=get_delete_url,
+        modules=admin.registered,
+    )
+
+
+@admin.route('/')
+def home():
+    return redirect(get_list_url('wallet'))
--- cashman/app.py
+++ cashman/app.py
@@ -4,9 +4,11 @@ import flask
 def create_app():
     from cashman import models
     from cashman.views import views
+    from cashman.admin import admin
     app = flask.Flask(__name__)
 
     app.config.from_pyfile('settings.py', silent=True)
     app.register_blueprint(views)
+    app.register_blueprint(admin, url_prefix='/admin')
     models.db.init_app(app)
     return app
